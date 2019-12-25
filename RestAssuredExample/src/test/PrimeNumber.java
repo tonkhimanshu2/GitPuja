a285d12cdcc74b7cfd74f0b3fe6def6bd3b3074d
minky <minky@Minky-PC>
2019-12-25 15:02:43 +0100

    Commit 4

--- RestAssuredExample/src/test/PrimeNumber.java
+++ RestAssuredExample/src/test/PrimeNumber.java
@@ -20,5 +20,7 @@ public class PrimeNumber {
 	       }	
 	       System.out.println("Prime numbers from 1 to 100 are :");
 	       System.out.println(primeNumbers);
+	       System.out.println("Got this data from Puja branch");
+	       
 	   }
 	}
